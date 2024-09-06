70e8c70e3d08158175f8bd251a11c98b57f56cd5
kenta-afk <kentarou9.60@icloud.com>
2024-09-06 12:14:22 +0900

    record tableの追加

--- app/Models/User.php
+++ app/Models/User.php
@@ -11,6 +11,10 @@ class User extends Authenticatable
 {
     use HasFactory, Notifiable;
 
+    public function records()
+    {
+        return $this->hasMany(Record::class);
+    }
     /**
      * The attributes that are mass assignable.
      *
